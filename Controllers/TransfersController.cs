dfe0b83b43932d9f35d40f30ccdd236882a82824
ShevHet <141827983+ShevHet@users.noreply.github.com>
2025-07-30 19:17:14 +0300

    2

--- Controllers/TransfersController.cs
+++ Controllers/TransfersController.cs
@@ -4,24 +4,22 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AccountService.Controllers
 {
-	[ApiController]
-	[Route("api/transfers")]
-	public class TransfersController : ControllerBase
-	{
-		private readonly IMediator _mediator;
+    [ApiController]
+    [Route("api/transfers")]
+    public class TransfersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
 
-		public TransfersController(IMediator mediator) => _mediator = mediator;
+        public TransfersController(IMediator mediator) => _mediator = mediator;
 
-		/// <summary>
-		/// Выполнить перевод между счетами
-		/// </summary>
-		[HttpPost]
-		[ProducesResponseType(204)]
-		[ProducesResponseType(400)]
-		public async Task<IActionResult> Transfer([FromBody] TransferCommand command)
-		{
-			await _mediator.Send(command);
-			return NoContent();
-		}
-	}
+        
+        [HttpPost]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> Transfer([FromBody] TransferCommand command)
+        {
+            await _mediator.Send(command);
+            return NoContent();
+        }
+    }
 }
